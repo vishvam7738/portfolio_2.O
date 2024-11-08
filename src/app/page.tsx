e4c07ffc3ec17b0584c9321ca0dbc0856d56f57c
vishvam <vishvamaggarwal@gmail.com>
2024-11-08 17:24:47 +0530

    Added Spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Grid from "@/components/grid";
+import Hero from "@/components/Hero";
+// import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="mt-24">
+      <Hero/>
       <Grid />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -66,7 +66,22 @@ module.exports = {
       gridTemplateRows: {
         "1-64": "repeat(1, minmax(64px, 1fr))",
         "2-64": "repeat(2, minmax(64px, 1fr))",
-      }
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
